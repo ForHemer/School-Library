2fa5816b08f1816a888d4e3725fba37e468f0484
ForHemer <foreste.hemerson@gmail.com>
2022-05-11 06:14:59 -0400

    Create and update Teacher.rb file

---
+++ Teacher.rb
@@ -0,0 +1,18 @@
+require "./Person.rb"
+
+class Student < Person
+    #Define constructor
+    def initialize(specialization)
+        #Call attributes from parent
+        super(age, name = "Unknown", parent_permission = true)
+        @specialization = specialization
+    end
+
+    #Define getter and setter for @specialization
+    attr_accessor :specialization
+
+    #Override can_use_services? method
+    def can_use_services?
+        return true
+    end
+end

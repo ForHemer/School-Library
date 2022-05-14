0c298790c3be88d78b279eb06f9a35d1d35e43fd
ForHemer <foreste.hemerson@gmail.com>
2022-05-13 21:35:11 -0400

    Create Book class

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+  # Define constructor
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  # Define getter and setter
+  attr_accessor :title, :author
+
+  def add_rental(date, person)
+    Rental.new(date, person, self)
+  end
+end

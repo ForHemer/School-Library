85ea8391f07e464eaf23be76bd40b4af73f43299
ForHemer <foreste.hemerson@gmail.com>
2022-05-13 23:12:33 -0400

    Define options

--- app.rb
+++ app.rb
@@ -16,3 +16,27 @@ def list_options
     6- List all rentals for a given person id
     7- Exit"
 end
+
+# Define options
+def options(books, patron, rentals)
+  loop do
+    list_options
+    option = gets.chomp.to_i
+    case option
+    when 1
+      books_list(books)
+    when 2
+      patrons_list(patron)
+    when 3
+      create_person(patron)
+    when 4
+      create_book(books)
+    when 5
+      create_rental(books, rentals, patron)
+    when 6
+      rental_list_by_id(rentals)
+    else
+      break
+    end
+  end
+end

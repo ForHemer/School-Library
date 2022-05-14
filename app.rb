19c5559859aad3509261e2e848a5d3d1bf0c5a3a
ForHemer <foreste.hemerson@gmail.com>
2022-05-13 23:15:35 -0400

    Define create_person method

--- app.rb
+++ app.rb
@@ -40,3 +40,30 @@ def options(books, patron, rentals)
     end
   end
 end
+
+# Define create_person method
+def create_person(patron)
+  print 'Do you want to create a student (1) or a teacher (2): '
+  choice = gets.chomp.to_i
+  print 'Age: '
+  age = gets.chomp.to_i
+  print 'Name: '
+  name = gets.chomp
+  case choice
+  when 1
+    print 'Has parent permission? [Y/N]: '
+    input = gets.chomp
+    case input.upcase
+    when 'Y'
+      permission = true
+    when 'N'
+      permission = false
+    end
+    patron.push(Student.new(age, name: name, parent_permission: permission))
+  when 2
+    print 'Specialization: '
+    specialization = gets.chomp
+    patron.push(Teacher.new(specialization, age, name))
+  end
+  puts 'Person created successfully.'
+end

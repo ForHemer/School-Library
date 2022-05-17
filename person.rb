05da6257e4f1ff298756102d80965d5f000af0f2
ForHemer <foreste.hemerson@gmail.com>
2022-05-17 13:00:47 -0400

    Update person.rb file

--- person.rb
+++ person.rb
@@ -2,38 +2,34 @@ require_relative 'decorator'
 require_relative 'rental'
 
 class Person < Nameable
-  # Define constructor
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
     super()
-    @id = Random.rand(1..1_000_000)
+    @id = rand(1..10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @rentals = []
   end
 
-  # Define getters and setters for @name and @age
-  attr_accessor :name, :age, :rentals
-  # Define getter @id
-  attr_reader :id
-
-  # Define correct_name method
   def correct_name
     @name
   end
-
-  # Define private method
-  private
 
   def of_age?
     @age >= 18
   end
 
   def can_use_services?
-    is_of_age? || @parent_permission = true
+    is_of_age? || @parent_permission
   end
 
   def add_rental(date, book)
     Rental.new(date, book, self)
   end
+
+  private :of_age?
+  public :can_use_services?
 end
